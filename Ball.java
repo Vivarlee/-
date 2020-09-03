8236001b3fbce6b1de305642c48c02c163338a6d
Vivarlee <58261056+Vivarlee@users.noreply.github.com>
2020-09-03 15:33:05 +0300

    Delete Ball.java

--- Ball.java
+++
@@ -1,9 +0,0 @@
-public class Ball {
-    public String type;
-    public double diameter;
-    public String colour;
-
-    public String toString() {
-        return "ball type: " + type+ "diameter= " + diameter + "colour "+colour;
-    }
-}
